07ded40aa5665b2f10f5490db6def4bfda12c9a6
Thomas Hawley <thawley22@turing.edu>
2023-02-04 11:08:13 -0700

    Complete includes? method

--- lib/linked_list.rb
+++ lib/linked_list.rb
@@ -78,4 +78,11 @@ class LinkedList
     end
     string.join(" ")
   end
+  def includes?(data)
+    current_node = @head
+    until current_node.data == data || current_node.next_node == nil
+      current_node = current_node.next_node
+    end
+    current_node.data == data
+  end
 end
