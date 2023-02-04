ac2d1a970a6e4c95ddad7e9bd291eca9cd70790f
Thomas Hawley <thawley22@turing.edu>
2023-02-04 14:13:12 -0700

    Create append method

--- lib/jungle_beat.rb
+++ lib/jungle_beat.rb
@@ -3,4 +3,12 @@ class JungleBeat
   def initialize
     @list = LinkedList.new
   end
+
+  def append(data)
+    split_data = data.split(" ")
+    split_data.each do |data|
+      @list.append(data)
+    end
+    data
+  end
 end
