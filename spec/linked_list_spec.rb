05cc62ffb72a9fbad33812409df70613a62267cc
Thomas Hawley <thawley22@turing.edu>
2023-02-04 10:54:02 -0700

    Create includes? test

--- spec/linked_list_spec.rb
+++ spec/linked_list_spec.rb
@@ -144,4 +144,18 @@ RSpec.describe LinkedList do
       expect(list.find(1, 3)).to eq("woo shi shu")
     end
   end
+  describe '#includes?' do
+    it 'can return true or false if' do
+      list = LinkedList.new
+      list.append("deep")
+      list.append("woo")
+      list.append("shi")
+      list.append("shu")
+      list.append("blop")
+
+      expect(list.includes?("deep")).to be(true)
+      expect(list.includes?("dep" )).to be(false)
+      
+    end
+  end
 end
