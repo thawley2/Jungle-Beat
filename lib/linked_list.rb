81741caac8b27c5c0be12b75290b4649850e75c7
Thomas Hawley <thawley22@turing.edu>
2023-02-03 18:10:28 -0700

    Create prepend and insert methods

--- lib/linked_list.rb
+++ lib/linked_list.rb
@@ -1,5 +1,5 @@
 class LinkedList 
-  attr_reader :head
+  attr_reader :head, :count
   def initialize
     @head = nil
   end
@@ -39,5 +39,29 @@ class LinkedList
       current_node = current_node.next_node 
     end
     string.join(" ")
+  end
+
+  def prepend(data)
+    #Need to add the head node as next node to the prepended node 
+    #and set all that to @head
+    @head = Node.new(data, @head)
+  end
+
+  #need to iterate through list to find the "index" position
+  #add inserted node to previous node's 'next_node'
+  #add the inserted nodes 'next_node' equal to the node at that 'index' location
+  def insert(local, data)
+    if @head == nil
+      @head = Node.new(data)
+      "There is nothing in the list yet, so your new data was added to the head of the list, you are welcome."
+    elsif local > count
+      "The list is not that long, please choose a new index position that is less than #{count}"
+    else 
+      current_node = @head
+      local.times do
+        current_node = current_node.next_node
+      end
+
+    end
   end
 end
