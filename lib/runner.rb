139c07b58cc756ae9a965d787f46998efae7d572
Thomas Hawley <thawley22@turing.edu>
2023-02-07 11:07:37 -0700

    Add runner file

---
+++ lib/runner.rb
@@ -0,0 +1,33 @@
+require_relative 'node'
+require_relative 'linked_list'
+require_relative 'jungle_beat'
+
+jb = JungleBeat.new ('deep doo ditt woo hoo shu')
+
+jb.rate = 150
+jb.voice = 'Bad News'
+jb.play
+
+=begin
+Here is a list of voices to choose from
+Female Voices
+- Kathy
+
+Male Voices
+- Fred
+- Junior
+- Ralph
+
+Novelty Voices
+- Albert
+- Bad News
+- Bahh
+- Bells
+- Boing
+- Bubbles
+- Cellos
+- Good News
+- Trinoids
+- Whisper
+- Zarvox
+=end
